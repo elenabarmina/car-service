3790fb5ed0110a3325a31497917bb4e4e13bf5c3
pechen <elenabarmina@yandex.ru>
2017-12-13 02:20:02 +0500

    repair view, fix individual, company: delete center

---
+++ modules/web/src/com/company/carservice/web/repair/RepairEdit.java
@@ -0,0 +1,7 @@
+package com.company.carservice.web.repair;
+
+import com.haulmont.cuba.gui.components.AbstractEditor;
+import com.company.carservice.entity.Repair;
+
+public class RepairEdit extends AbstractEditor<Repair> {
+}
