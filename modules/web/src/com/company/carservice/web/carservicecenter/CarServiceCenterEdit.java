b5a75e56966ef64df064888dd46e81af81b61312
pechen <elenabarmina@yandex.ru>
2017-12-16 03:56:14 +0500

    ds listenet customers count added

--- modules/web/src/com/company/carservice/web/carservicecenter/CarServiceCenterEdit.java
+++ modules/web/src/com/company/carservice/web/carservicecenter/CarServiceCenterEdit.java
@@ -4,8 +4,11 @@ import com.company.carservice.service.CityService;
 import com.haulmont.cuba.gui.components.AbstractEditor;
 import com.company.carservice.entity.CarServiceCenter;
 import com.haulmont.cuba.gui.components.GroupBoxLayout;
+import com.haulmont.cuba.gui.data.CollectionDatasource;
+import com.haulmont.cuba.gui.data.impl.ValueCollectionDatasourceImpl;
 
 import javax.inject.Inject;
+import java.util.Map;
 
 public class CarServiceCenterEdit extends AbstractEditor<CarServiceCenter> {
 
@@ -15,10 +18,18 @@ public class CarServiceCenterEdit extends AbstractEditor<CarServiceCenter> {
     @Inject
     GroupBoxLayout customerBox;
 
+    @Inject
+    CollectionDatasource customerDs;
+
     @Override
     protected void initNewItem(CarServiceCenter item) {
         item.setCity(cityService.getDefaultCity());
     }
 
-
+    @Override
+    public void init(Map<String, Object> params) {
+        customerDs.addCollectionChangeListener(event -> {
+            customerBox.setCaption("Customers (" + String.valueOf(customerDs.getItemIds().size()) + ")");
+        });
+    }
 }
