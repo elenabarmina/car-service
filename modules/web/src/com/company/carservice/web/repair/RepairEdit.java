920c675faca75e19b0cd84e7b50c6ae39fd98205
pechen <elenabarmina@yandex.ru>
2017-12-16 16:49:54 +0500

    screen repair edit - employers depending from center

--- modules/web/src/com/company/carservice/web/repair/RepairEdit.java
+++ modules/web/src/com/company/carservice/web/repair/RepairEdit.java
@@ -1,7 +1,32 @@
 package com.company.carservice.web.repair;
 
-import com.haulmont.cuba.gui.components.AbstractEditor;
+import com.company.carservice.entity.CarServiceCenter;
+import com.company.carservice.entity.Employee;
+import com.haulmont.cuba.gui.components.*;
 import com.company.carservice.entity.Repair;
+import com.haulmont.cuba.gui.data.CollectionDatasource;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.util.Map;
+import java.util.UUID;
 
 public class RepairEdit extends AbstractEditor<Repair> {
+
+    @Inject
+    CollectionDatasource<Employee, UUID> employeesDs;
+
+    @Inject
+    CollectionDatasource<CarServiceCenter, UUID> centersDs;
+
+    @Named("fieldGroup.employeeField")
+    Field employeeField;
+
+    public void init(Map<String, Object> params){
+
+        centersDs.addItemChangeListener(event -> {
+            employeeField.setValue(null);
+            employeesDs.refresh();
+        });
+    }
 }
